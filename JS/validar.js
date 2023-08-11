3dbbc2df53a44f7d80b8b19ff808e2e54b82b1ea
sebastiancll <sebastiancll483@gmail.com>
2023-08-11 11:54:25 -0500

    version 11.08.2023

--- JS/validar.js
+++ JS/validar.js
@@ -1,10 +1,10 @@
 function validar(){
     var pulsado = false;
-    var tipo_jor = document.form-validacion.opc;
+    var tipo_jor = document.formulario.opc;
     var elegido = -1;
-    var opcion = document.form-validacion.condiciones;
+    var opcion = document.formulario.condiciones;
     for (i = 0; i < tipo_jor.length; i++) {
-       if (tipo_jor[i].checked ==true){
+       if (tipo_jor[i].checked == true){
         pulsado = true
         elegido = i
        } 
@@ -19,8 +19,8 @@ function validar(){
         swal("Oops!", "El formulario no ha podido enviarse. \n \n Debe aceptar las condiciones para poder enviar el formulario", 'error');
         return false; 
     }     
-    else {o.
+    else {
         miOpcion = tipo_jor[elegido].value
-        swal("Perfecto","Has elegido la jornada: " + miOpcion + "\n \n El formulario ha sido enviado.",'success');   
+        swal("Perfecto","Has aceptado tu homosexualidad: " + miOpcion + "\n \n El formulario ha sido enviado.",'success');   
     } 
 }
