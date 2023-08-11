1bdd1eb4b359222fec03851b6b2e2627fb06365b
sebastiancll <sebastiancll483@gmail.com>
2023-08-11 11:27:28 -0500

    version 11.08.2023

---
+++ JS/validar.js
@@ -0,0 +1,26 @@
+function validar(){
+    var pulsado = false;
+    var tipo_jor = document.form-validacion.opc;
+    var elegido = -1;
+    var opcion = document.form-validacion.condiciones;
+    for (i = 0; i < tipo_jor.length; i++) {
+       if (tipo_jor[i].checked ==true){
+        pulsado = true
+        elegido = i
+       } 
+    }
+
+    if (pulsado != true) {
+        swal("Oops!", "No has elegido si eres gey. \n \n Elige una opccion para continuar", 'error');
+        return false 
+    }
+
+    if (opcion.checked != true) {
+        swal("Oops!", "El formulario no ha podido enviarse. \n \n Debe aceptar las condiciones para poder enviar el formulario", 'error');
+        return false; 
+    }     
+    else {o.
+        miOpcion = tipo_jor[elegido].value
+        swal("Perfecto","Has elegido la jornada: " + miOpcion + "\n \n El formulario ha sido enviado.",'success');   
+    } 
+}
